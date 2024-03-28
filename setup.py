d68fd716ec7c23b35979ec061bab5204c3bbd8a5
wojiushizzl <47440786+wojiushizzl@users.noreply.github.com>
2024-03-28 17:46:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import ruamel.yaml
+
 
 current_path = os.getcwd()
 
@@ -8,8 +10,15 @@ logo_path = os.path.abspath('./setup/logo.png')
 sh_path = os.path.abspath('./setup/start.sh')
 
 dt_path = os.path.abspath('./setup/STARTAPP.desktop')
+
 userapp_path = os.path.abspath('./App_user')
+
 devapp_path = os.path.abspath('./App_dev')
+
+
+usr_path=os.listdir('/home')
+print(f"usr name :{usr_path[0]}")
+init_tran_yaml_path=os.path.join('/home',usr_path[0],'.config','Ultralytics','settings.yaml')
 
 print(current_path)
 print(logo_path)
@@ -53,4 +62,14 @@ app_dir = '/usr/share/applications'
 shutil.copy(dt_path, app_dir)
 
 # modify init yaml path at usr/home/.config/ultralytics
-# to be continue...
+yaml = ruamel.yaml.YAML()
+# 读取复制的文件并更改其中一行代码
+with open(init_tran_yaml_path, "r", encoding="utf-8") as file:
+    lines = yaml.load(file)
+
+if "datasets_dir" in lines:
+    lines["datasets_dir"] = devapp_path
+
+# 将更改后的内容写回文件
+with open(init_tran_yaml_path, "w", encoding="utf-8") as file:
+    yaml.dump(lines, file)
