2d134b90f0162e719eceb5b34911a3d5fc152087
wojiushizzl <47440786+wojiushizzl@users.noreply.github.com>
2024-03-28 15:45:40 +0800

    Update logic_check.py

--- App_user/logic_check.py
+++ App_user/logic_check.py
@@ -1,7 +1,7 @@
 LOGIC = [
-    "L10 flange",
-    "HS Pachage",
-    "HS Rolling"
+    "Detected -> OUTPUT",
+    "No Detected -> OUTPUT",
+
 ]
 
 
@@ -10,11 +10,8 @@ def logic_check(logic, boxes):
         num = len(list(boxes.cls))
         result = True if num > 0 else False
         return result
+
     if LOGIC.index(logic) == 1:
-        num = len(list(boxes.cls))
-        result = True if num > 0 else False
-        return result
-    if LOGIC.index(logic) == 2:
         num = len(list(boxes.cls))
         result = False if num > 0 else True
         return result
