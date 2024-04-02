dcfabda37f817d4ebdb9c56b8fe3a4243761707b
wojiushizzl <47440786+wojiushizzl@users.noreply.github.com>
2024-04-02 20:34:40 +0800

    update

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ import subprocess
 current_path = os.getcwd()
 
 logo_path = os.path.abspath('./setup/logo.png')
+dev_logo_path=os.path.abspath('./setup/dev_logo.png')
 
-sh_path = os.path.abspath('./setup/start.sh')
+user_sh_path = os.path.abspath('./setup/start.sh')
+dev_sh_path=os.path.abspath('./setup/dev.sh')
+
 
 dt_path = os.path.abspath('./setup/STARTAPP.desktop')
+dev_dt_path = os.path.abspath('./setup/DEVAPP.desktop')
 
 userapp_path = os.path.abspath('./App_user')
-
 devapp_path = os.path.abspath('./App_dev')
 
 
@@ -22,13 +25,13 @@ init_tran_yaml_path=os.path.join('/home',usr_path[0],'.config','Ultralytics','se
 
 print(current_path)
 print(logo_path)
-print(sh_path)
+print(user_sh_path)
 print(userapp_path)
 print(devapp_path)
 print(dt_path)
 
-# modify .sh file
-with open(sh_path, 'r') as f:
+# modify start.sh file
+with open(user_sh_path, 'r') as f:
     sh_lines = f.readlines()
 
 cd_command = 'cd ' + userapp_path + '\n'
@@ -47,10 +50,34 @@ try:
 except:
     print('No conda detected !')
 
-with open(sh_path, 'w') as f:
+with open(user_sh_path, 'w') as f:
     f.writelines(sh_lines)
 
-# modify .desktop file
+# modify dev.sh file
+with open(dev_sh_path, 'r') as f:
+    sh_lines = f.readlines()
+
+cd_command = 'cd ' + devapp_path + '\n'
+for i in range(len(sh_lines)):
+    if 'cd' in sh_lines[i]:
+        sh_lines[i] = cd_command
+
+try:
+    condash_path=subprocess.check_output(['locate','conda.sh'])
+    condash_path=condash_path.decode().split('\n')[0]
+    source_command='source '+condash_path +'\n'
+
+    for i in range(len(sh_lines)):
+        if 'source' in sh_lines[i]:
+            sh_lines[i] = source_command
+except:
+    print('No conda detected !')
+
+with open(dev_sh_path, 'w') as f:
+    f.writelines(sh_lines)
+
+
+# modify STARTAPP.desktop file
 with open(dt_path, 'r') as f:
     dt_lines = f.readlines()
 
@@ -59,17 +86,40 @@ iconkey = 'Icon='
 
 for i in range(len(dt_lines)):
     if dt_lines[i].startswith(EXEkey):
-        dt_lines[i] = EXEkey + sh_path + '\n'
+        dt_lines[i] = EXEkey + user_sh_path + '\n'
     if dt_lines[i].startswith(iconkey):
         dt_lines[i] = iconkey + logo_path + '\n'
 
 with open(dt_path, 'w') as f:
     f.writelines(dt_lines)
+# modify DEVAPP.desktop file
+with open(dev_dt_path, 'r') as f:
+    dt_lines = f.readlines()
+
+EXEkey = 'Exec=bash '
+iconkey = 'Icon='
+
+for i in range(len(dt_lines)):
+    if dt_lines[i].startswith(EXEkey):
+        dt_lines[i] = EXEkey + dev_sh_path + '\n'
+    if dt_lines[i].startswith(iconkey):
+        dt_lines[i] = iconkey + dev_logo_path + '\n'
+
+with open(dev_dt_path, 'w') as f:
+    f.writelines(dt_lines)
+
+
 
 # copy .desktop to /usr/share/applications/
 app_dir = '/usr/share/applications'
 
 shutil.copy(dt_path, app_dir)
+
+shutil.copy(dev_dt_path, app_dir)
+
+
+
+
 
 # modify init yaml path at usr/home/.config/ultralytics
 yaml = ruamel.yaml.YAML()
