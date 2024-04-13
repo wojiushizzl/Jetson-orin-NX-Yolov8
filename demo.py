295c8557221bda84e1d19348a5088d607196ccc3
wojiushizzl <47440786+wojiushizzl@users.noreply.github.com>
2024-04-13 10:57:50 +0800

    add jetson.inference demo

---
+++ demo.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+from jetson_utils import videoSource,videoOutput,cudaToNumpy,cudaFromNumpy
+import torch
+import torchvision
+
+print(torch.__version__)
+print(torchvision.__version__)
+
+model = YOLO('yolov8n.pt')
+camera=videoSource('/dev/video0')
+display=videoOutput('display://0')
+
+while display.IsStreaming():
+    cuda_img=camera.Capture()
+    if cuda_img is None:
+        continue
+    array_img=cudaToNumpy(cuda_img)
+    res=model.predict(array_img,conf=0.5)
+    array_res=res[0].plot()
+    cuda_res=cudaFromNumpy(array_res)
+
+    display.Render(cuda_res)
+    display.SetStatus("YOLOV8 DEECTION ")
+
+
+# Load the YOLOv8 model
